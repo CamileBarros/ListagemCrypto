2fda135ca88599002dd37f7094839dac031e4fce
Camile Barros <camile.barros@warren.com.br>
2022-04-13 19:04:50 -0300

    Listagem crypto - update BottomNavigationBar

---
+++ lib/home_controller.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeController extends InheritedNotifier<ValueNotifier<int>> {
+  HomeController({Key? key, required Widget child})
+      : super(
+          key: key,
+          child: child,
+        );
+}
---
+++ lib/myapp.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'home_controller.dart';
+import 'home_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeController(child: HomePage()),
+    );
+  }
+}
