fc1a86fcc9e104700cbd006921df97742469cdaf
camilebarroswarren <camile.barros@warren.com.br>
2022-04-23 12:48:23 -0300

    update layout homePage

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -8,6 +8,8 @@ class HomeWalletScreen extends StatefulWidget {
   State<HomeWalletScreen> createState() => _HomeWalletScreenState();
 }
 
+bool show = true;
+
 class _HomeWalletScreenState extends State<HomeWalletScreen> {
   final List<CryptoListModel> containerDatas = [
     CryptoListModel("BTC", "BitCoin", 5000, 30),
@@ -17,7 +19,43 @@ class _HomeWalletScreenState extends State<HomeWalletScreen> {
 
   @override
   Widget build(BuildContext context) {
+    void _visibility(bool visibility) {
+      setState(() => show = visibility);
+    }
+
     return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(200),
+        child: Container(
+          height: 200,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Carteira",
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+                AnimatedOpacity(
+                  opacity: show ? 1 : 0,
+                  duration: Duration(milliseconds: 500),
+                  curve: Curves.easeInOut,
+                  child: Text(
+                    "R\$ 2000,00",
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.visibility),
+                  alignment: Alignment.topLeft,
+                  onPressed: () => _visibility(!show),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
       body: Column(children: [
         ...containerDatas
             .map((e) => Container(
